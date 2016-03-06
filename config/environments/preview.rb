b61006afb4719f46838a0a4d16b36214e58d70b2
Godwin <goodgodwin@hotmail.com>
2016-03-06 14:13:09 -0800

    Fixed issue in preview config

--- config/environments/preview.rb
+++ config/environments/preview.rb
@@ -1,4 +1,4 @@
-BikeBike::Application.configure do
+Rails.application.configure do
 	# Settings specified here will take precedence over those in config/application.rb.
 
 	# Code is not reloaded between requests.
